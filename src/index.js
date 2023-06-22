f20c9fce7aee453fbcd57bfd58b6d404573a1ce4
Vanessa Soares <vanis.sbs@gmail.com>
2023-06-22 15:15:30 -0300

    Apostila06(Ciclo de vida): Exercício ciclo de vida
    - Criação componente principal App
    - Arquivo README.md

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/vela-blue/theme.css';
+import 'primeflex/primeflex.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { Card } from 'primereact/card';
+        
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+export default function App() {
+ return (
+    <Card title="JOGO DE CONTINHAS" className="md:w-25rem bg-cyan-700 shadow-8">
+        <Card className='bg-cyan-50'>
+
+        </Card>
+    </Card>
+ )
+}
+ReactDOM.render(
+ <App />,
+ document.querySelector("#root")
+)
